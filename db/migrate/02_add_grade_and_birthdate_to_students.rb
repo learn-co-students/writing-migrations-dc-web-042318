73256a3e145c2da3bcf05ef94cb259291b9454e3
github username <github email address>
2018-05-07 16:38:22 -0400

    Done.

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,7 +1,7 @@
-class CreateStudents < ActiveRecord::Migration[4.2]
+class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[4.2]
 
   def change
-    add_column t.integer :grade
-    add_column t.string :birthdate
+    add_column :students, :grade, :integer
+    add_column :students, :birthdate, :string
   end
 end
